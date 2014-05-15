94a6d01d7b6fa3e317df0fde07707d49a6ab9ea4
Luciano Frizzera <lucaju@me.com>
2014-05-14 23:49:11 -0600

    Version 2.01

--- bin-debug/php/getWorkflows.php
+++ bin-debug/php/getWorkflows.php
@@ -8,27 +8,14 @@ if($_POST['action']) {
 	require_once("functions.php");
 	
 	$dbConn = DBConn::getConnection();
-	
-	//get data
-	$wdata = stripslashes($_POST['wdata']);
-	
-	//JSON decode
-	$jData = json_decode($wdata,true);
-	
-	//save data - Workflow
-	$userID = $jData['userID'];
-	
-	//define query based on the user ID
-	/*
-	if ($userID > 0) {
-		$query = "SELECT * FROM workflow ORDER BY id DESC";
+
+	//define query based on the user ID	
+	if (isset($_POST['userID'])) {
+		$userID = $_POST['userID'];
+		$query = "SELECT * FROM workflow WHERE user_id='$userID' ORDER BY id DESC";
 	} else {
-		$query = "SELECT * FROM workflow WHERE user_id = '$userID' ORDER BY id DESC";
+		$query = "SELECT * FROM workflow WHERE visibility='1' ORDER BY id DESC";
 	}
-	*/
-	
-	
-	$query = "SELECT * FROM workflow ORDER BY id DESC";
 	
 	if ($result = $dbConn->query($query)) {
 	 	
@@ -43,9 +30,8 @@ if($_POST['action']) {
 	 		
 		 		$queryAuthor = "SELECT id, first_name, last_name FROM users WHERE id = '$userID'";
 		 		
-		 		
 		 		if ($resultAuthor = $dbConn->query($queryAuthor)) {
-		 			
+
 		 			$authorInfo = $resultAuthor->fetch_assoc();
 		 			$author = $authorInfo['first_name']." ".$authorInfo['last_name'];
 		 			
@@ -56,7 +42,7 @@ if($_POST['action']) {
 	 		//save info to send
 	 	
 	 		$row['title'] = utf8_encode($row['title']);
-	 		$row['author'] = $author;
+	 		$row['author'] = utf8_encode($author);
 	 		
 			$rows[] = $row;
 		}
@@ -70,8 +56,6 @@ if($_POST['action']) {
 	
 	/* close connection */
 	$dbConn->close();
-
-   
 
 }
 
